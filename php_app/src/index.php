57d35587654da5515b4dfb8fdfa54e6e98f16cbc
= <=>
2019-12-20 18:13:43 -0300

    Atualizando docker file para construir a imagem dinamicamente

--- php_app/src/index.php
+++ php_app/src/index.php
@@ -1,1 +1,16 @@
-echo("smkadkamdaksmdlkamsl");
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Uma página em PHP</title>
+</head>
+
+<body>
+    <h1>Uma página em PHP que funciona dentro dessa mesma aplicação</h1>
+    <?php echo (random_int(0, 1000)); ?>
+</body>
+
+</html>
